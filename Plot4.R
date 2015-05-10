01799488f746cebf277a5e9f6e3cc216e1328124
ssubbaiah <ssubbaiah@yahoo.com>
2015-05-10 18:15:17 -0500

    Modified Plot4 and renamed files

--- Plot4.R
+++ Plot4.R
@@ -23,15 +23,27 @@ SM1 <- as.numeric(data$Sub_metering_1)
 SM2 <- as.numeric(data$Sub_metering_2)
 SM3 <- as.numeric(data$Sub_metering_3)
 volt <- as.numeric(data$Voltage)
+grp <- as.numeric(data$Global_reactive_power)
 
-png(".\\ExpAnalysis_P1_Data\\plot3.png",480,480)
+
+# Set Graph
+png(".\\ExpAnalysis_P1_Data\\plot4.png",480,480)
+par(mfrow = c(2, 2))
+#Plot 1
+plot(dategpa,gpa, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+# plot 2
+plot(dategpa, volt, type="l", xlab="datetime", ylab="Voltage")
+#Plot 3
 with (data, plot(dategpa,SM1, type="l", ylab="Energy Sub metering", xlab=""))
 with (subset(data, 1==1), points(dategpa, SM2,type="l",col="red"))
 with (subset(data, 1==1), points(dategpa, SM3,type="l",col="blue"))
 legend("topright", col=c("black","red","blue"), c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), lwd=c(1,1))
+#Plot 4
+plot(dategpa, grp, type="l", xlab="datetime", ylab="Global_reactive_power")
+
 dev.off()
 
-# Check for existence of Plot3.png
-if  (file.exists(".\\ExpAnalysis_P1_Data\\Plot3.png")) {  print ("Plot3.png created successfully!") } else { print ("*****ERROR*****: Plot3.png does not exist, please check!!!")     }
+# Check for existence of Plot4.png
+if  (file.exists(".\\ExpAnalysis_P1_Data\\Plot4.png")) {  print ("Plot4.png created successfully!") } else { print ("*****ERROR*****: Plot4.png does not exist, please check!!!")     }
 
-#end of code Plot3.R.
+#end of code Plot4.R.
